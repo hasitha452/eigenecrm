b137a79647f265c135baf9ad49a894f1e01a0dc8
Hasitha <hasitha4520@gmail.com>
2017-06-22 22:55:20 +0530

    ok banku

---
+++ contactdetail.php
@@ -0,0 +1,90 @@
+<?php
+include_once './header.php';
+?>
+
+<div class="static-content-wrapper">
+    <div class="static-content">
+        <div class="page-content">
+
+            <div class="container-fluid">
+                <div data-widget-group="group1">
+
+                    <div class="panel panel-default" data-widget='{"draggable": "false"}'>
+                        <div class="panel-heading">
+                            <h2>Contact Details - <b style="color: #990000;">1 of 999</b></h2>
+                            <div class="panel-ctrls" data-actions-container="" data-action-collapse='{"target": ".panel-body"}'></div>
+                        </div>
+                        <div class="panel-editbox" data-widget-controls=""></div>
+                        <div class="panel-body">
+                            <div class="col-sm-12" style="background-color: #b9f6ca;">
+                                <div class="col-sm-6">
+                                    <h4><b>Peter Smith Rathnayaka</b></h4><br>
+                                    <p>Siemens AG</p>
+                                    <p>Teststreet 1-99</p><br>
+                                    <p>12345 Test City</p>
+                                    <p>Germany</p>
+
+
+                                </div>
+                                <div class="col-sm-6" >
+                                    <h4>Customer</h4>
+                                    <p>Phone : +49123423424</p>
+                                    <p>FAX&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; : +49123423423</p>
+                                    <br>
+                                    <p>etc ......</p>
+
+                                </div>
+                            </div>
+
+                            <div class="col-sm-12">
+
+                                <hr style="border: #660033 1px solid;">
+
+                                <ul class="nav nav-tabs">
+                                    <li class="active"><a data-toggle="tab" href="#home">History</a></li>
+                                    <li><a data-toggle="tab" href="#menu1">Task</a></li>
+                                    <li><a data-toggle="tab" href="#menu2">Opportunities</a></li>
+                                    <li><a data-toggle="tab" href="#menu4">Group</a></li>
+                                    <li><a data-toggle="tab" href="#menu3">More Tabs</a></li>
+                                </ul>
+
+                                <div class="tab-content">
+                                    <div id="home" class="tab-pane fade in active">
+                                        <h3>HOME</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                    </div>
+                                    <div id="menu1" class="tab-pane fade">
+                                        <h3>Menu 1</h3>
+                                        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                    </div>
+                                    <div id="menu2" class="tab-pane fade">
+                                        <h3>Menu 2</h3>
+                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                    </div>
+                                    <div id="menu4" class="tab-pane fade">
+                                        <h3>Menu 4</h3>
+                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+                                    </div>
+                                    <div id="menu3" class="tab-pane fade">
+                                        <h3>Menu 3</h3>
+                                        <p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                                    </div>
+                                </div>
+
+
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div> <!-- .container-fluid -->
+        </div> <!-- #page-content -->
+    </div>
+
+    <?php
+    include_once './footer.php';
+    ?>
